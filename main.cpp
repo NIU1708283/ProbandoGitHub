a8a3a92438bacd2a33dd508349c5ed99d7786b0d
Felipe Tenório Da Silva <1708283@uab.cat>
2024-03-31 18:24:26 +0200

    segundo commit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/// no se que decirte la verdad
+// ? que deberia hacer?
+// ñ: buena pregunta
+// cada uno con un color
+// ! tengo miedo
+// * todo tachado 
+
+
 int main()
 {
-
+	int a = 0;
+	cout << a << endl;
+	// ñ: hola que tal bitch
 	return 0;
 }
